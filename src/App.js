0d70f79cb7ae8fc04b3ea2cb6ac25eddd0348f9e
Bertrand Payet <bertrand@Bertrands-MBP.fritz.box>
2020-06-06 17:32:23 +0200

    another positive change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Hello world. I was there with git
+           Hello world. I was there with git. another
         </p>
         <a
           className="App-link"
